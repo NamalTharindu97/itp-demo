d5495d07002b9d2c4fc03408443c02cf119ce3e3
NamalTharindu97 <NamalA701997@gmail.com>
2023-06-23 15:22:17 +0530

    search text input add

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-	return <div className="nav-container"></div>;
+	return (
+		<div className="nav-container">
+			<div className="search-container">
+				<input
+					className="search-class"
+					type="text"
+					placeholder="Search"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
