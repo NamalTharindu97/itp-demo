f70a54b474094a0c5d16532019f421657085fe9c
NamalTharindu97 <NamalA701997@gmail.com>
2023-06-23 15:42:19 +0530

    sider bar row styles

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./SideBar.css";
 
 const SideBar = () => {
-	return <div className="side-bar"></div>;
+	return (
+		<div className="side-bar">
+			<div className="table">
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+				<div className="row">row 1</div>
+			</div>
+		</div>
+	);
 };
 
 export default SideBar;
